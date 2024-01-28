cf408f199e20523e0e68c8a6dd43b4ab14ad4600
MrMasterKeyboard <73440604+AFellowSpeedrunner@users.noreply.github.com>
2024-01-28 18:41:09 +0000

    Add very rough fade out code.

--- scripts/background.js
+++ scripts/background.js
@@ -28,7 +28,7 @@ setInterval(() => {
                             { src: '/images/settings-icon.png', sizes: '256x256', type: 'image/png' }
                         ]
                     });
-                };
+                }
             } else if (regex.shopping.test(url)) {
                 if (audio.paused) {
                     audio.src = audioSource.shopping;
@@ -40,19 +40,32 @@ setInterval(() => {
                             { src: '/images/eshop-icon.png', sizes: '256x256', type: 'image/png' }
                         ]
                     });
-                };
+                }
             } else if (!regex.settings.test(url) && !regex.shopping.test(url) && !audio.paused && !audio.src.includes(audioSource.health)) {
-                audio.pause();
-            };
+                fadeOut();
+            }
         });
 }, 1000);
+
+function fadeOut() {
+    let volume = audio.volume;
+    const fadeOutInterval = setInterval(() => {
+        if (volume > 0) {
+            volume -= 0.001;
+            audio.volume = volume;
+        } else {
+            clearInterval(fadeOutInterval);
+            audio.pause();
+            audio.volume = 1;
+        }
+    }, 1);
+}
 
 browser.runtime.onMessage.addListener((message) => {
     if (message.type === 'updateSettingsAudio') {
         audioSource.settings = '/audio/' + message.source;
-        audioSource.settings = '/audio/' + message.source;
         audio.src = audioSource.settings;
-    };
+    }
 });
 
 browser.runtime.onMessage.addListener((message) => {
@@ -60,14 +73,14 @@ browser.runtime.onMessage.addListener((message) => {
         if (audioSource.shopping != '/audio/' + message.source) {
             audioSource.shopping = '/audio/' + message.source;
             audio.src = audioSource.shopping;
-        };
-    };
+        }
+    }
 });
 
 browser.runtime.onMessage.addListener((message) => {
     if (message.type === 'updateVolume') {
         audio.volume = message.volume / 100;
-    };
+    }
 });
 
 browser.runtime.onInstalled.addListener((details) => {
@@ -76,7 +89,7 @@ browser.runtime.onInstalled.addListener((details) => {
             type: 'basic',
             iconUrl: browser.extension.getURL('/images/warning.png'),
             title: 'WARNING - HEALTH AND SAFETY',
-            message: 'BEFORE PLAYING, READ THE HEALTH AND SAFETY PRECAUTIONS BOOKLET FOR IMPORTANT INFORMATION ABOUT YOUR HEALTH AND SAFETY.',
+            message: 'BEFORE PLAYING, READ THE HEALTH AND SAFETY PRECAUTIONS BOOKLET FOR IMPORTANT INFORMATION ABOUT YOUR HEALTH AND SAFETY.'
         };
 
         browser.notifications.create(notificationOptions, (notificationId) => {
@@ -88,7 +101,7 @@ browser.runtime.onInstalled.addListener((details) => {
         });
 
         audio.src = audioSource.health;
-        audio.volume = 0.1;
+        audio.volume = 50;
         audio.play();
     }
 });
